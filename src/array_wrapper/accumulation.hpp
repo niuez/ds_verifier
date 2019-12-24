0d58c23770a4225bfaaf57e4411f95f20c030efe
niuez <niuez.graph@gmail.com>
2019-12-25 08:54:50 +0900

    [add] accumlation wrapper

---
+++ src/array_wrapper/accumulation.hpp
@@ -0,0 +1,34 @@
+#ifndef ARRAY_WRAPPER_ACCUM_HPP
+#define ARRAY_WRAPPER_ACCUM_HPP
+
+#include <vector>
+#include <data_structures/accumulation.hpp>
+#include <array_wrapper.hpp>
+
+namespace ds {
+
+  template<class T>
+  class array_wrapper<T, accumulation<T>> {
+    
+
+  public:
+    using size_type = std::size_t;
+    using value_type = T;
+
+  private:
+    accumulation<value_type> accum;
+
+  public:
+    array_wrapper() {};
+    array_wrapper(const std::vector<T>& init) : accum(init) {}
+
+    value_type accum_from0(size_type r) const {
+      return accum.accum_from0(r);
+    }
+    size_type size() const {
+      return accum.size();
+    }
+  };
+}
+
+#endif
---
+++ src/data_structures/accumulation.hpp
@@ -0,0 +1,38 @@
+#ifndef DATA_STRUCTURES_ACCUM_HPP
+#define DATA_STRUCTURES_ACCUM_HPP
+
+#include <vector>
+
+namespace ds {
+  template<class T>
+  class accumulation {
+  public:
+
+    using value_type = T;
+    using size_type = std::size_t;
+
+  private:
+
+    const size_type N;
+    std::vector<value_type> arr;
+
+  public:
+
+    accumulation(): N(0), arr(1) {}
+    accumulation(const std::vector<value_type>& init)
+      : N(init.size()), arr(N + 1) {
+        for(size_type i = 0;i < init.size(); i++) {
+          arr[i + 1] = arr[i] + init[i];
+        }
+      }
+
+    value_type accum_from0(size_type r) const {
+      return arr[r];
+    }
+    size_type size() const {
+      return N;
+    }
+  };
+}
+
+#endif
