9ba50027023b09c0488e0ce5617adec013017cec
niuez <niuez.graph@gmail.com>
2019-12-26 22:03:50 +0900

    [add] skipping verify if file timestamp is older than verify timestamp

--- verify.py
+++ verify.py
@@ -3,6 +3,7 @@ from subprocess import (DEVNULL, PIPE, STDOUT, CalledProcessError,
     TimeoutExpired, call, check_call, check_output, run)
 from pathlib import Path
 from datetime import datetime
+import argparse
 
 def execute(exe: Path, source: str):
     verify_name = exe.stem
@@ -26,21 +27,48 @@ def execute(exe: Path, source: str):
                     "detail": str(e)
                 })
 
+def load_old_verify_results(path: Path):
+    try:
+        return json.load(open(path, 'r'))
+    except:
+        return {}
+
+def is_verify_skip(old_results, verify_source: Path):
+    verify_name = verify_source.stem
+    if verify_name in old_results:
+        old_timestamp = datetime.strptime(old_results[verify_name]["timestamp"], '%Y-%m-%d %H:%M:%S')
+        file_timestamp = datetime.fromtimestamp(verify_source.stat().st_ctime)
+        return old_timestamp > file_timestamp
+    return False
+
 if __name__ == '__main__':
 
-    verify_list = open('verify_list', 'r').read().split('\n')
-    verify_results = {}
+    parser = argparse.ArgumentParser(description='generate ds_verify result')
+    parser.add_argument('cmake_build_path', type=Path, default=None, help='path to cmake build directory')
+
+    parsed = parser.parse_args()
+
+    build_path = parsed.cmake_build_path
+
+    verify_list = open(build_path / "verify_list", 'r').read().split('\n')
+
+    verify_results = load_old_verify_results(".ds_verify/verify_results.json")
+
     for verify_task in verify_list[0:-1]:
         task_tmp = verify_task.split(':')
         verify_exe = task_tmp[0]
         verify_source = task_tmp[1]
 
-        print("verify: %s" % verify_source)
+        if is_verify_skip(verify_results, Path(verify_source)):
+            print("skipped: %s" % verify_source)
 
-        (verify_name, status) = execute(Path(verify_exe), verify_source)
-        verify_results[verify_name] = status
+        else:
+            print("verify: %s" % verify_source)
+
+            (verify_name, status) = execute(Path(verify_exe), verify_source)
+            verify_results[verify_name] = status
     
-    json.dump(verify_results, open("verify_results.json", 'w'), indent=2)
+    json.dump(verify_results, open(build_path / "verify_results.json", 'w'), indent=2)
 
     print("finish")
 
