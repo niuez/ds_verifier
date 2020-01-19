857315170f2af894712e6fff8667a6dc8b0d456f
niuez <niuez.graph@gmail.com>
2020-01-19 13:22:47 +0900

    [update] query::check(static) -> query.check(member) for Flexibility

--- src/queries/random_choise.hpp
+++ src/queries/random_choise.hpp
@@ -5,51 +5,72 @@
 
 namespace ds {
 
-  template<class... Queries>
+  template<class Q, class... Qs>
   class random_choise {
-
   public:
     using size_type = std::size_t;
+    using query_type = Q;
+    using qs_type = random_choise<Qs...>;
 
-    const static size_type query_size = sizeof...(Queries);
+    const static size_type query_size = sizeof...(Qs) + 1;
 
   private:
-
-    template<class Q, class... Qs>
-    struct Checker {
-      template<class Gen, class Target, class Checker>
-      static void checker(size_type n, Gen& gen, Target& target, Checker& checker) {
-        if(sizeof...(Qs) == n) Q::check(gen, target, checker);
-        else random_choise::Checker<Qs...>::template checker<Gen, Target, Checker>(n, gen, target, checker);
-      }
-      static std::string name() {
-        return "[" + std::string(Q::name()) + "], " + Checker<Qs...>::name();
-      }
-    };
-
-    template<class Q>
-    struct Checker<Q> {
-      template<class Gen, class Target, class Checker>
-      static void checker(size_type n, Gen& gen, Target& target, Checker& checker) {
-        assert(n == 0);
-        Q::check(gen, target, checker);
-      }
-      static std::string name() {
-        return "[" + std::string(Q::name()) + "]";
-      }
-    };
-
+    query_type query;
+    qs_type qs;
 
   public:
 
+    static std::string name_rec() {
+      return std::string("[") + query_type::name() + "], " + qs_type::name_rec();
+    }
+
     static std::string name() {
-      return std::string("random_choise from ") + Checker<Queries...>::name();
+      return std::string("random_choise from ") + name_rec();
     }
 
     template<class Gen, class Target, class Checker>
-    static void check(Gen& gen, Target& target, Checker& checker) {
+    void check_rec(size_type n, Gen& gen, Target& target, Checker& checker) {
+      if(sizeof...(Qs) == n) query.check(gen, target, checker);
+      else qs.template check_rec<Gen, Target, Checker>(n, gen, target, checker);
+    }
+
+    template<class Gen, class Target, class Checker>
+    void check(Gen& gen, Target& target, Checker& checker) {
       size_type qn = std::uniform_int_distribution<size_type>(0, query_size - 1)(gen);
-      random_choise::Checker<Queries...>::checker(qn, gen, target, checker);
+      this->template check_rec<Gen, Target, Checker>(qn, gen, target, checker);
+    }
+  };
+
+  template<class Q>
+  class random_choise<Q> {
+  public:
+    using size_type = std::size_t;
+    using query_type = Q;
+
+    const static size_type query_size = 1;
+
+  private:
+    query_type query;
+
+  public:
+
+    static std::string name_rec() {
+      return std::string("[") + query_type::name() + "]";
+    }
+
+    static std::string name() {
+      return std::string("random_choise from ") + name_rec();
+    }
+
+    template<class Gen, class Target, class Checker>
+    void check_rec(size_type n, Gen& gen, Target& target, Checker& checker) {
+      query.check(gen, target, checker);
+    }
+
+    template<class Gen, class Target, class Checker>
+    void check(Gen& gen, Target& target, Checker& checker) {
+      size_type qn = std::uniform_int_distribution<size_type>(0, query_size - 1)(gen);
+      this->template check_rec<Gen, Target, Checker>(qn, gen, target, checker);
     }
   };
 }
